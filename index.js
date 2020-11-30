8076d5fe9c292ea57dcb7bdf3ccd94645b77a95d
lichun <lchrennew@126.com>
2020-11-30 09:33:32 +0800

    v0.1.2

---
+++ index.js
@@ -0,0 +1,17 @@
+import DragonflyCanvas from "./src/components/DragonflyCanvas.vue";
+import DragonflyEdge from "./src/components/DragonflyEdge.vue";
+import DragonflyEndpoint from "./src/components/DragonflyEndpoint.vue";
+import StraightLine from "./src/components/edge/StraightLine.vue";
+import ZigZagLine from "./src/components/edge/ZigZagLine.vue";
+import SCurveLine from "./src/components/edge/SCurveLine.vue";
+import LineShapeBase from "./src/components/edge/LineShapeBase.vue";
+
+export default {
+    DragonflyCanvas,
+    DragonflyEdge,
+    DragonflyEndpoint,
+    StraightLine,
+    ZigZagLine,
+    SCurveLine,
+    LineShapeBase,
+}
