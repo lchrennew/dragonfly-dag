62c1f261dd383e388c549583db5eb00aa111181a
lichun <lchrennew@126.com>
2020-11-30 15:21:50 +0800

    v0.2.1

---
+++ rollup.publish.config.js
@@ -0,0 +1,37 @@
+import vuePlugin from 'rollup-plugin-vue'
+import {terser} from "rollup-plugin-terser"
+import generatePackageJson from 'rollup-plugin-generate-package-json'
+import copy from 'rollup-plugin-copy'
+
+export default {
+    input: 'index.js',
+    output: {
+        file: 'dist/index.js',
+        format: 'es',
+        name: 'dragonfly'
+    },
+    plugins: [
+        vuePlugin({
+            target: "browser",
+        }),
+        terser(),
+        generatePackageJson({
+            baseContents(pkg) {
+                return {
+                    ...pkg,
+                    main: 'index.js',
+                    module: 'index.js',
+                    devDependencies: {},
+                    scripts: {},
+                }
+            }
+        }),
+        copy({
+            targets: [
+                {src: 'src/**/*.less', dest: 'dist/'},
+            ]
+        })
+
+    ]
+
+}
